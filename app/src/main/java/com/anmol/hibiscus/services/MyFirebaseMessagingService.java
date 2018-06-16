1e15accebcce8fabcc8fd8e7533f54b6203d3d91
anmol2805 <saxenaanmol28@gmail.com>
2018-06-16 12:53:47 +0530

    notification channel added

--- app/src/main/java/com/anmol/hibiscus/services/MyFirebaseMessagingService.java
+++ app/src/main/java/com/anmol/hibiscus/services/MyFirebaseMessagingService.java
@@ -1,13 +1,17 @@
 package com.anmol.hibiscus.services;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.content.ContextCompat;
 
 import com.anmol.hibiscus.HibiscusActivity;
 import com.anmol.hibiscus.R;
@@ -21,27 +25,55 @@ import java.util.Map;
  */
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService{
+    String CHANNEL_ID = "canopy_notification_id";
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
+        createNotificationChannel();
         if(remoteMessage.getData().size()>0){
             Map<String,String> payload = remoteMessage.getData();
             showNotification(payload);
         }
     }
 
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String name = "canopy notification channel";
+            String description = "notifications to keep you updated about latest IIIT news";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            channel.setLightColor(channel.getLightColor());
+            channel.setVibrationPattern(new long[]{100,100,100,100});
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
     private void showNotification(Map<String, String> payload) {
         Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,CHANNEL_ID);
+        builder.setColorized(true);
+        builder.setColor(ContextCompat.getColor(this,R.color.colorPrimary));
+
         builder.setSmallIcon(R.drawable.hibif);
+        builder.setBadgeIconType(R.drawable.hibif);
+        builder.setWhen(System.currentTimeMillis());
         builder.setContentTitle(payload.get("title"));
         builder.setContentText(payload.get("body"));
         builder.setSound(alarmSound);
+        builder.setVibrate(new long[] {100,100,100,100});
+        builder.setPriority(NotificationCompat.PRIORITY_HIGH);
+        builder.setCategory(NotificationCompat.CATEGORY_MESSAGE);
+        builder.setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
         Intent resultIntent = new Intent(this,HibiscusActivity.class);
         TaskStackBuilder stackbuilder = TaskStackBuilder.create(this);
         stackbuilder.addNextIntent(resultIntent);
         PendingIntent resultPendingIntent = stackbuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
         builder.setContentIntent(resultPendingIntent);
-        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        builder.setAutoCancel(true);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
         notificationManager.notify(0,builder.build());
     }
 }
