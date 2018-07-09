ac92071530531788746dfd3baea83b125fb9d16c
anmol2805 <saxenaanmol28@gmail.com>
2018-07-09 12:20:29 +0530

    db testing

--- app/src/main/java/com/anmol/hibiscus/SplashActivity.java
+++ app/src/main/java/com/anmol/hibiscus/SplashActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,6 +20,7 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.anmol.hibiscus.Helpers.Dbhelper;
 import com.bumptech.glide.Glide;
 import com.anmol.hibiscus.Model.Notice;
 import com.anmol.hibiscus.services.RequestService;
@@ -71,44 +73,28 @@ public class SplashActivity extends AppCompatActivity {
         else{
 //            Intent intent = new Intent(this, RequestService.class);
 //            startService(intent);
-            mdatabase = FirebaseDatabase.getInstance().getReference().child("Notice");
-            final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Students").child(auth.getCurrentUser().getUid()).child("hibiscus");
+//            mdatabase = FirebaseDatabase.getInstance().getReference().child("Notice");
+//            final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Students").child(auth.getCurrentUser().getUid()).child("hibiscus");
             progressBar = (ProgressBar)findViewById(R.id.load);
             progressBar.setVisibility(View.VISIBLE);
-            notices = new ArrayList<>();
+            //notices = new ArrayList<>();
 
             img = (ImageView)findViewById(R.id.imageView2);
             animFadein = AnimationUtils.loadAnimation(getApplicationContext(),
                     R.anim.fade_in);
             zoomin = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
             img.startAnimation(zoomin);
-            Handler handler = new Handler();
+            loaddata();
+//            Handler handler = new Handler();
+//
+//
 //            handler.postDelayed(new Runnable() {
 //                @Override
 //                public void run() {
-//                    Intent intent = new Intent(SplashActivity.this, RequestServiceAttendance.class);
-//                    startService(intent);
-//
-//
-//                }
-//            },1000);
-//            handler.postDelayed(new Runnable() {
-//                @Override
-//                public void run() {
-//                    Intent intent = new Intent(SplashActivity.this, RequestServiceCourses.class);
-//                    startService(intent);
-//
-//
-//                }
-//            },2000);
-
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    progressBar.setVisibility(View.INVISIBLE);
-                    Intent intent = new Intent(SplashActivity.this, HibiscusActivity.class);
-                    startActivity(intent);
-                    overridePendingTransition(R.anim.still,R.anim.slide_in_up);
+//                    progressBar.setVisibility(View.INVISIBLE);
+//                    Intent intent = new Intent(SplashActivity.this, HibiscusActivity.class);
+//                    startActivity(intent);
+//                    overridePendingTransition(R.anim.still,R.anim.slide_in_up);
 //                    databaseReference.addValueEventListener(new ValueEventListener() {
 //                        @Override
 //                        public void onDataChange(DataSnapshot dataSnapshot) {
@@ -133,12 +119,96 @@ public class SplashActivity extends AppCompatActivity {
 //                    });
 //                    Intent intent = new Intent(SplashActivity.this, RequestServiceGrades.class);
 //                    startService(intent);
-                }
-            },3000);
+//                }
+//            },3000);
 
         }
 
 
+    }
+
+    private void loaddata() {
+        final JSONObject jsonObject = new JSONObject();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().getRoot();
+        databaseReference.child("Students").child(auth.getCurrentUser().getUid()).child("hibiscus").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child("sid").getValue(String.class)!=null && dataSnapshot.child("pwd").getValue(String.class)!=null){
+                    uid = dataSnapshot.child("sid").getValue(String.class);
+                    pwd = dataSnapshot.child("pwd").getValue(String.class);
+                    try {
+                        jsonObject.put("uid",uid);
+                        jsonObject.put("pwd",pwd);
+                        jsonObject.put("pass","encrypt");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, getResources().getString(R.string.notice_url), jsonObject, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+
+                            try {
+                                Dbhelper dbhelper = new Dbhelper(SplashActivity.this);
+                                int c = 0;
+                                while (c<response.getJSONArray("Notices").length()){
+
+                                    JSONObject object = response.getJSONArray("Notices").getJSONObject(c);
+
+
+                                    title = object.getString("title");
+                                    date = object.getString("date");
+                                    postedby = object.getString("posted_by");
+                                    attention = object.getString("attention");
+                                    id = object.getString("id");
+                                    Notice notice = new Notice(title,date,postedby,attention,id);
+                                    dbhelper.insertData(notice);
+                                    dbhelper.updatenotice(notice);
+                                    c++;
+                                }
+                                progressBar.setVisibility(View.INVISIBLE);
+                                Intent intent = new Intent(SplashActivity.this, HibiscusActivity.class);
+                                startActivity(intent);
+                                overridePendingTransition(R.anim.still,R.anim.slide_in_up);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            try {
+                                JSONObject object0 = response.getJSONArray("Notices").getJSONObject(0);
+
+                                title = object0.getString("title");
+                                date = object0.getString("date");
+                                postedby = object0.getString("posted_by");
+                                attention = object0.getString("attention");
+                                id = object0.getString("id");
+                                Notice notice = new Notice(title,date,postedby,attention,id);
+                                FirebaseDatabase.getInstance().getReference().child("Notices").setValue(notice);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            progressBar.setVisibility(View.INVISIBLE);
+                            Intent intent = new Intent(SplashActivity.this, HibiscusActivity.class);
+                            startActivity(intent);
+                            overridePendingTransition(R.anim.still,R.anim.slide_in_up);
+                        }
+                    });
+                    Mysingleton.getInstance(getApplicationContext()).addToRequestqueue(jsonObjectRequest);
+
+
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
 
