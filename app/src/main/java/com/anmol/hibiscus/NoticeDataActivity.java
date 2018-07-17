d0e9f6919ff9fe52ed882f0a68a81152e89bac3e
anmol2805 <saxenaanmol28@gmail.com>
2018-07-17 11:49:00 +0530

    waiting for tesing

--- app/src/main/java/com/anmol/hibiscus/NoticeDataActivity.java
+++ app/src/main/java/com/anmol/hibiscus/NoticeDataActivity.java
@@ -1,6 +1,8 @@
 package com.anmol.hibiscus;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +21,8 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.anmol.hibiscus.Helpers.Dbhelper;
+import com.anmol.hibiscus.Model.Notice;
 import com.anmol.hibiscus.Model.Noticedata;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
@@ -33,6 +37,9 @@ import com.google.firebase.database.ValueEventListener;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NoticeDataActivity extends AppCompatActivity {
     String id,uid,pwd,title,date,att,post;
@@ -58,26 +65,25 @@ public class NoticeDataActivity extends AppCompatActivity {
         setTitle("Notices");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        retry = (Button)findViewById(R.id.retry);
+        retry = (Button) findViewById(R.id.retry);
         //data = (TextView)findViewById(R.id.data);
-        nd = (WebView)findViewById(R.id.nd);
-        d = (TextView)findViewById(R.id.date);
-        a = (TextView)findViewById(R.id.attention);
-        p = (TextView)findViewById(R.id.posted);
-        t = (TextView)findViewById(R.id.title);
-        pn = (ProgressBar)findViewById(R.id.pn);
+        nd = (WebView) findViewById(R.id.nd);
+        d = (TextView) findViewById(R.id.date);
+        a = (TextView) findViewById(R.id.attention);
+        p = (TextView) findViewById(R.id.posted);
+        t = (TextView) findViewById(R.id.title);
+        pn = (ProgressBar) findViewById(R.id.pn);
         fail = (ImageView) findViewById(R.id.fail);
         final InterstitialAd interstitialAdnotice = new InterstitialAd(this);
         interstitialAdnotice.setAdUnitId("ca-app-pub-5827006149924215/9013567438");
         interstitialAdnotice.loadAd(new AdRequest.Builder().build());
-        interstitialAdnotice.setAdListener(new AdListener(){
+        interstitialAdnotice.setAdListener(new AdListener() {
             @Override
             public void onAdLoaded() {
                 super.onAdLoaded();
-                if(interstitialAdnotice.isLoaded()){
+                if (interstitialAdnotice.isLoaded()) {
                     interstitialAdnotice.show();
-                }
-                else {
+                } else {
                     loadview();
                 }
             }
@@ -88,12 +94,12 @@ public class NoticeDataActivity extends AppCompatActivity {
                 loadview();
             }
         });
-        if(interstitialAdnotice.isLoaded()){
+        if (interstitialAdnotice.isLoaded()) {
             interstitialAdnotice.show();
-        }
-        else {
+        } else {
             loadview();
         }
+
     }
 
     private void loadview() {
@@ -114,11 +120,10 @@ public class NoticeDataActivity extends AppCompatActivity {
         nd.getSettings().setUseWideViewPort(true);
         nd.getSettings().setTextZoom(175);
         nd.setInitialScale(1);
-        id = getIntent().getStringExtra("id");
-        title = getIntent().getStringExtra("title");
-        date = getIntent().getStringExtra("date");
-        att = getIntent().getStringExtra("att");
-        post = getIntent().getStringExtra("posted");
+
+        handleIntent(getIntent());
+
+
         d.setText(date);
         p.setText(post);
         a.setText(att);
@@ -184,6 +189,51 @@ public class NoticeDataActivity extends AppCompatActivity {
 
     }
 
+    private void handleIntent(Intent intent) {
+        // ATTENTION: This was auto-generated to handle app links.
+        Uri appLinkData = intent.getData();
+        if(appLinkData!=null){
+            id = appLinkData.getLastPathSegment();
+            String query = "Select * from notice_table WHERE notice_id="+id;
+            Dbhelper dbhelper = new Dbhelper(this);
+            List<Notice> notices = new ArrayList<>();
+            notices.clear();
+            notices = dbhelper.readData(query);
+            try{
+                title = notices.get(0).getTitle();
+                date = notices.get(0).getDate();
+                att = notices.get(0).getAttention();
+                post = notices.get(0).getPosted_by();
+                System.out.println(notices);
+            }
+            catch (IndexOutOfBoundsException e){
+                e.printStackTrace();
+                List<Notice> notices1 = dbhelper.readData("Select * from notice_table ORDER BY notice_id DESC");
+                ArrayList<String> noticeids = new ArrayList<>();
+                for(int i = 0;i<notices1.size();i++){
+                    noticeids.add(notices1.get(i).getId());
+                }
+                loaddata(noticeids,dbhelper,id);
+
+            }
+
+
+        }
+        else{
+            id = intent.getStringExtra("id");
+            title = getIntent().getStringExtra("title");
+            date = getIntent().getStringExtra("date");
+            att = getIntent().getStringExtra("att");
+            post = getIntent().getStringExtra("posted");
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleIntent(intent);
+    }
+
     private void request(JSONObject object, final int i) {
         System.out.println(object);
         retry.setVisibility(View.GONE);
@@ -239,7 +289,98 @@ public class NoticeDataActivity extends AppCompatActivity {
         });
         Mysingleton.getInstance(NoticeDataActivity.this).addToRequestqueue(jsonObjectRequest);
     }
+    private void loaddata(final ArrayList<String> noticeids, final Dbhelper dbhelper, final String id) {
+        final JSONObject jsonObject = new JSONObject();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().getRoot();
+        databaseReference.child("Students").child(auth.getCurrentUser().getUid()).child("hibiscus").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child("sid").getValue(String.class)!=null && dataSnapshot.child("pwd").getValue(String.class)!=null){
+                    uid = dataSnapshot.child("sid").getValue(String.class);
+                    pwd = dataSnapshot.child("pwd").getValue(String.class);
+                    try {
+                        jsonObject.put("uid",uid);
+                        jsonObject.put("pwd",pwd);
+                        jsonObject.put("pass","encrypt");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, getResources().getString(R.string.notice_url), jsonObject, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
 
+                            try {
+                                int c = 0;
+                                while (c<response.getJSONArray("Notices").length()){
+
+                                    JSONObject object = response.getJSONArray("Notices").getJSONObject(c);
+
+
+                                    String mtitle = object.getString("title");
+                                    String mdate = object.getString("date");
+                                    String postedby = object.getString("posted_by");
+                                    String attention = object.getString("attention");
+                                    String mid = object.getString("id");
+                                    Notice notice = new Notice(mtitle,mdate,postedby,attention,mid);
+                                    int k=0;
+                                    for(int j = 0;j<noticeids.size();j++){
+                                        if(noticeids.get(j).equals(NoticeDataActivity.this.id)){
+                                            k=1;
+                                        }
+                                    }
+                                    if(k==0){
+                                        System.out.print("noticestatus:new entry");
+                                        dbhelper.insertData(notice);
+                                    }
+                                    else{
+                                        System.out.print("noticestatus:already present");
+                                    }
+                                    dbhelper.updatenotice(notice);
+                                    c++;
+                                }
+                                String query = "Select * from notice_table WHERE notice_id="+id;
+                                List<Notice> notices = new ArrayList<>();
+                                notices.clear();
+                                notices = dbhelper.readData(query);
+                                try{
+                                    title = notices.get(0).getTitle();
+                                    date = notices.get(0).getDate();
+                                    att = notices.get(0).getAttention();
+                                    post = notices.get(0).getPosted_by();
+                                    System.out.println(notices);
+                                }
+                                catch (IndexOutOfBoundsException e){
+                                    e.printStackTrace();
+
+                                }
+
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                        }
+                    });
+                    Mysingleton.getInstance(getApplicationContext()).addToRequestqueue(jsonObjectRequest);
+
+
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
