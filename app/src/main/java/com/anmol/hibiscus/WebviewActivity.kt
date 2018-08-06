10de8284d8be7f9c60a0785f431c5cf0cceb8b67
anmol2805 <saxenaanmol28@gmail.com>
2018-08-06 10:09:07 +0530

    webview activity started

---
+++ app/src/main/java/com/anmol/hibiscus/WebviewActivity.kt
@@ -0,0 +1,12 @@
+package com.anmol.hibiscus
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WebviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_webview)
+    }
+}
