c158da5bee301d67cea6376645dc086d2f0edf64
anmol2805 <saxenaanmol28@gmail.com>
2018-08-06 10:19:54 +0530

    webview completed

--- app/src/main/java/com/anmol/hibiscus/WebviewActivity.kt
+++ app/src/main/java/com/anmol/hibiscus/WebviewActivity.kt
@@ -2,11 +2,40 @@ package com.anmol.hibiscus
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebSettings
+import kotlinx.android.synthetic.main.activity_webview.*
 
 class WebviewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_webview)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+        webview.isFocusable = true
+        webview.isFocusableInTouchMode = true
+        webview.settings.javaScriptEnabled = true
+        webview.settings.javaScriptCanOpenWindowsAutomatically = true
+        webview.settings.loadsImagesAutomatically = true
+        webview.settings.setSupportZoom(true)
+        webview.settings.builtInZoomControls = true
+        webview.settings.setRenderPriority(WebSettings.RenderPriority.HIGH)
+        //webview.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ONLY);
+        webview.settings.domStorageEnabled = true
+        webview.settings.databaseEnabled = true
+        webview.settings.loadsImagesAutomatically = true
+        //webview.getSettings().setAppCacheEnabled(true);
+        webview.scrollBarStyle = View.SCROLLBARS_INSIDE_OVERLAY
+        webview.settings.useWideViewPort = true
+        webview.settings.textZoom = 175
+        webview.setInitialScale(1)
+        webview.loadUrl("https://www.cryptohype.live/")
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
 }
