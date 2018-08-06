6fe330f2220bdb4e9d51a7b024c8c1a54b42da60
anmol2805 <saxenaanmol28@gmail.com>
2018-08-06 10:41:36 +0530

    webview completed testing done

--- app/src/main/java/com/anmol/hibiscus/WebviewActivity.kt
+++ app/src/main/java/com/anmol/hibiscus/WebviewActivity.kt
@@ -1,10 +1,16 @@
 package com.anmol.hibiscus
 
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.webkit.WebSettings
 import kotlinx.android.synthetic.main.activity_webview.*
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+
 
 class WebviewActivity : AppCompatActivity() {
 
@@ -28,9 +34,20 @@ class WebviewActivity : AppCompatActivity() {
         //webview.getSettings().setAppCacheEnabled(true);
         webview.scrollBarStyle = View.SCROLLBARS_INSIDE_OVERLAY
         webview.settings.useWideViewPort = true
-        webview.settings.textZoom = 175
-        webview.setInitialScale(1)
-        webview.loadUrl("https://www.cryptohype.live/")
+//        webview.settings.textZoom = 175
+
+        webview.run {
+            setInitialScale(1)
+            loadUrl(intent.getStringExtra("weburl"))
+            webViewClient = object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        view.loadUrl(request.url.toString())
+                    }
+                    return false
+                }
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
