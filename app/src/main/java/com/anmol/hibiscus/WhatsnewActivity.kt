13b699c64cc22fd594b67115fcf8fc3f951f7ef7
anmol2805 <saxenaanmol28@gmail.com>
2018-08-12 23:25:16 +0530

    whats new activity started

---
+++ app/src/main/java/com/anmol/hibiscus/WhatsnewActivity.kt
@@ -0,0 +1,12 @@
+package com.anmol.hibiscus
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WhatsnewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_whatsnew)
+    }
+}
