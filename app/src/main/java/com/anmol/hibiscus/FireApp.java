dc637a7aecafcbbb575de96adbfacc2bf1f5087e
anmol2805 <saxenaanmol28@gmail.com>
2018-08-19 11:53:55 +0530

    18-28

--- app/src/main/java/com/anmol/hibiscus/FireApp.java
+++ app/src/main/java/com/anmol/hibiscus/FireApp.java
@@ -1,6 +1,8 @@
 package com.anmol.hibiscus;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.FirebaseDatabase;
@@ -10,6 +12,11 @@ import com.google.firebase.database.FirebaseDatabase;
  */
 
 public class FireApp extends Application {
+    @Override
+    protected void attachBaseContext(Context context) {
+        super.attachBaseContext(context);
+        MultiDex.install(this);
+    }
     @Override
     public void onCreate() {
         super.onCreate();
