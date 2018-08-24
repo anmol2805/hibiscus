c45e529fe411f93b8b1968d6be707ad39d76e4c1
anmol2805 <saxenaanmol28@gmail.com>
2018-08-24 10:16:13 +0530

    rosei fragment added

---
+++ app/src/main/java/com/anmol/hibiscus/Rasoi/rosei.java
@@ -0,0 +1,21 @@
+package com.anmol.hibiscus.Rasoi;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.anmol.hibiscus.R;
+
+public class rosei extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View vi = inflater.inflate(R.layout.activity_rosei,container,false);
+
+        return vi;
+    }
+}
