8812add4946d0a572754b0a0642516202d98a7ca
anmol2805 <saxenaanmol28@gmail.com>
2018-09-23 00:56:24 +0530

    auth bug resolved

--- app/src/main/java/com/anmol/hibiscus/services/RequestService.java
+++ app/src/main/java/com/anmol/hibiscus/services/RequestService.java
@@ -13,9 +13,11 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.anmol.hibiscus.Adapter.Grades;
+import com.anmol.hibiscus.LoginActivity;
 import com.anmol.hibiscus.Model.Attendance;
 import com.anmol.hibiscus.Model.Notice;
 import com.anmol.hibiscus.Mysingleton;
+import com.anmol.hibiscus.NoticeDataActivity;
 import com.anmol.hibiscus.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -58,80 +60,90 @@ public class RequestService extends IntentService {
         auth = FirebaseAuth.getInstance();
         databaseReference = FirebaseDatabase.getInstance().getReference().getRoot();
         noticedatabase = FirebaseDatabase.getInstance().getReference().child("Notice");
-        gradesdatabase = FirebaseDatabase.getInstance().getReference().child("Students").child(auth.getCurrentUser().getUid()).child("grades");
-        attendancedatabase = FirebaseDatabase.getInstance().getReference().child("Students").child(auth.getCurrentUser().getUid()).child("attendance");
-        final JSONObject jsonObject = new JSONObject();
-        databaseReference.child("Students").child(auth.getCurrentUser().getUid()).child("hibiscus").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                if(dataSnapshot.child("sid").getValue(String.class)!=null && dataSnapshot.child("pwd").getValue(String.class)!=null){
-                    uid = dataSnapshot.child("sid").getValue(String.class);
-                    pwd = dataSnapshot.child("pwd").getValue(String.class);
-                    try {
-                        jsonObject.put("uid",uid);
-                        jsonObject.put("pwd",pwd);
-                        jsonObject.put("pass","encrypt");
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, getResources().getString(R.string.notice_url), jsonObject, new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
+        if(auth.getCurrentUser()!=null){
+            gradesdatabase = FirebaseDatabase.getInstance().getReference().child("Students").child(auth.getCurrentUser().getUid()).child("grades");
+            attendancedatabase = FirebaseDatabase.getInstance().getReference().child("Students").child(auth.getCurrentUser().getUid()).child("attendance");
+            final JSONObject jsonObject = new JSONObject();
+            databaseReference.child("Students").child(auth.getCurrentUser().getUid()).child("hibiscus").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if(dataSnapshot.child("sid").getValue(String.class)!=null && dataSnapshot.child("pwd").getValue(String.class)!=null){
+                        uid = dataSnapshot.child("sid").getValue(String.class);
+                        pwd = dataSnapshot.child("pwd").getValue(String.class);
+                        try {
+                            jsonObject.put("uid",uid);
+                            jsonObject.put("pwd",pwd);
+                            jsonObject.put("pass","encrypt");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, getResources().getString(R.string.notice_url), jsonObject, new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
 
-                            try {
-                                int c = 0;
-                                while (c<response.getJSONArray("Notices").length()){
+                                try {
+                                    int c = 0;
+                                    while (c<response.getJSONArray("Notices").length()){
 
-                                    JSONObject object = response.getJSONArray("Notices").getJSONObject(c);
+                                        JSONObject object = response.getJSONArray("Notices").getJSONObject(c);
 
 
-                                    title = object.getString("title");
-                                    date = object.getString("date");
-                                    postedby = object.getString("posted_by");
-                                    attention = object.getString("attention");
-                                    id = object.getString("id");
-                                    Notice notice = new Notice(title,date,postedby,attention,id,false,false);
-                                    //notices.add(notice);
-                                    noticedatabase.child(String.valueOf(c)).setValue(notice);
-                                    c++;
+                                        title = object.getString("title");
+                                        date = object.getString("date");
+                                        postedby = object.getString("posted_by");
+                                        attention = object.getString("attention");
+                                        id = object.getString("id");
+                                        Notice notice = new Notice(title,date,postedby,attention,id,false,false);
+                                        //notices.add(notice);
+                                        noticedatabase.child(String.valueOf(c)).setValue(notice);
+                                        c++;
+                                    }
+
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
                                 }
+                                try {
+                                    JSONObject object0 = response.getJSONArray("Notices").getJSONObject(0);
 
-
-                            } catch (JSONException e) {
-                                e.printStackTrace();
+                                    title = object0.getString("title");
+                                    date = object0.getString("date");
+                                    postedby = object0.getString("posted_by");
+                                    attention = object0.getString("attention");
+                                    id = object0.getString("id");
+                                    Notice notice = new Notice(title,date,postedby,attention,id,false,false);
+                                    FirebaseDatabase.getInstance().getReference().child("Notices").setValue(notice);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
                             }
-                            try {
-                                JSONObject object0 = response.getJSONArray("Notices").getJSONObject(0);
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
 
-                                title = object0.getString("title");
-                                date = object0.getString("date");
-                                postedby = object0.getString("posted_by");
-                                attention = object0.getString("attention");
-                                id = object0.getString("id");
-                                Notice notice = new Notice(title,date,postedby,attention,id,false,false);
-                                FirebaseDatabase.getInstance().getReference().child("Notices").setValue(notice);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
                             }
-                        }
-                    }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-
-                        }
-                    });
-                    Mysingleton.getInstance(getApplicationContext()).addToRequestqueue(jsonObjectRequest);
+                        });
+                        Mysingleton.getInstance(getApplicationContext()).addToRequestqueue(jsonObjectRequest);
 
 
+
+                    }
 
                 }
 
-            }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
+                }
+            });
+        }
+        else{
+            FirebaseAuth.getInstance().signOut();
+            Intent intent2 = new Intent(getApplicationContext(),LoginActivity.class);
+            intent2.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent2.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent2);
+        }
 
-            }
-        });
     }
 }
